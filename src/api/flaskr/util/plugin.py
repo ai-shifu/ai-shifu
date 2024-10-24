4d0aca1c2bd5a525e9d1b17a8f8336001f2c7e02
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-24 17:09:55 +0800

    feat: add simple plugin command

--- src/api/flaskr/util/plugin.py
+++ src/api/flaskr/util/plugin.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask
 from inspect import isfunction, getmembers
 from functools import wraps, partial
+import subprocess
+import shutil
+import click
 
 
 def inject(func):
@@ -49,3 +52,51 @@ def load_plugins_from_dir(app: Flask, plugins_dir: str):
         load_from_directory(plugins_dir)
 
     return plugins
+
+
+def enable_plugins(app: Flask):
+
+    @app.cli.group()
+    def plugin():
+        """Plugin management commands."""
+        pass
+
+    @plugin.command(name="add")
+    @click.argument("repo_url")
+    def add(repo_url):
+        """Add a plugin by cloning the repository."""
+        repo_name = repo_url.split("/")[-1].replace(".git", "")
+        dest_dir = os.path.join("flaskr", "plugins", repo_name)
+        if os.path.exists(dest_dir):
+            print(f"Plugin {repo_name} already exists.")
+            return
+        subprocess.run(["git", "clone", repo_url, dest_dir])
+        print(f"Plugin {repo_name} added.")
+
+    @plugin.command(name="delete")
+    @click.argument("repo_name")
+    def delete(repo_name):
+        """Delete a plugin by its repository name."""
+        dest_dir = os.path.join("flaskr", "plugins", repo_name)
+        if not os.path.exists(dest_dir):
+            print(f"Plugin {repo_name} does not exist.")
+            return
+        shutil.rmtree(dest_dir)
+        print(f"Plugin {repo_name} deleted.")
+
+    @plugin.command(name="list")
+    def list():
+        """List all plugins."""
+        plugins_dir = os.path.join("flaskr", "plugins")
+        plugins = [
+            name
+            for name in os.listdir(plugins_dir)
+            if os.path.isdir(os.path.join(plugins_dir, name))
+        ]
+        print("Installed plugins:")
+        for plugin in plugins:
+            if plugin == "__pycache__":
+                continue
+            print(f"- {plugin}")
+
+    return plugin
