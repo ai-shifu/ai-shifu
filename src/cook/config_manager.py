ca998c979d500ab3aa01aa74e8197c71a43c0718
Kenrick-Zhou <Kenrick.Zhou@gmail.com>
2024-10-24 16:25:08 +0800

    refactor: Img uploader related adapt to the overall project docker.

--- src/cook/config_manager.py
+++ src/cook/config_manager.py
@@ -53,16 +53,12 @@ class ConfigManager:
             _cfg_lark = config["lark"]
             self.DEF_LARK_VIEW_ID = _cfg_lark["default_view_id"]
 
-            _cfg_fileupload = config["fileupload"]
-            self.IMG_LOCAL_DIR = _cfg_fileupload["img_local_dir"]
-            (
-                os.makedirs(self.IMG_LOCAL_DIR)
-                if not os.path.exists(self.IMG_LOCAL_DIR)
-                else None
-            )
-            self.IMG_OSS_ANAME = _cfg_fileupload["img_oss_aname"]
-            self.IMG_OSS_ENDPOINT = _cfg_fileupload["img_oss_endpoint"]
-            self.IMG_OSS_BUCKET = _cfg_fileupload["img_oss_bucket"]
+            # img fileupload
+            self.IMG_LOCAL_DIR = os.getenv("COOK_IMG_LOCAL_DIR")
+            os.makedirs(self.IMG_LOCAL_DIR, exist_ok=True)
+            self.IMG_OSS_ANAME = os.getenv("COOK_IMG_OSS_ANAME")
+            self.IMG_OSS_ENDPOINT = os.getenv("COOK_IMG_OSS_ENDPOINT")
+            self.IMG_OSS_BUCKET = os.getenv("COOK_IMG_OSS_BUCKET")
             self.OSS_ACCESS_KEY_ID = os.getenv("ALIBABA_CLOUD_OSS_ACCESS_KEY_ID")
             self.OSS_ACCESS_KEY_SECRET = os.getenv("ALIBABA_CLOUD_OSS_ACCESS_KEY_SECRET")
 
