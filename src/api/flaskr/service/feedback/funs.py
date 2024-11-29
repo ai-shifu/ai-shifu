d135d0c6beb9c508ad2f93b686936145539afc92
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-11-29 14:27:17 +0800

    fix: update feedback info

--- src/api/flaskr/service/feedback/funs.py
+++ src/api/flaskr/service/feedback/funs.py
@@ -7,7 +7,7 @@ from flaskr.service.user.models import User
 
 def submit_feedback(app: Flask, user_id: str, feedback: str):
     with app.app_context():
-        feedback = FeedBack(user_id=user_id, feedback=feedback)
+        feedback_item = FeedBack(user_id=user_id, feedback=feedback)
         user = User.query.filter(User.user_id == user_id).first()
         if user:
             send_notify(
@@ -20,6 +20,6 @@ def submit_feedback(app: Flask, user_id: str, feedback: str):
                     f"用户反馈：{feedback}",
                 ],
             )
-        db.session.add(feedback)
+        db.session.add(feedback_item)
         db.session.commit()
-        return feedback.id
+        return feedback_item.id
