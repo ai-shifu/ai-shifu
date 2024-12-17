449963a97eb13e46aba2e131ffdfb8cebe7a6a80
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-12-17 17:20:21 +0800

    feat: merge logo image

--- src/web/src/Components/logo/LogoWithText.jsx
+++ src/web/src/Components/logo/LogoWithText.jsx
@@ -1,10 +1,6 @@
 import { memo } from 'react';
-import logoBlue from 'Assets/logos/logo-blue-120.png';
-import logoColor from 'Assets/logos/logo-color-120.png';
-import logoWhite from 'Assets/logos/logo-white-120.png';
-import logoTextRow from 'Assets/logos/logo-text-horiz-160.png';
-import logoTextColumn from 'Assets/logos/logo-text-verti-160.png';
-import logoTextRowWhite from 'Assets/logos/logo-text-horiz-white-160.png';
+import logoTextRow from 'Assets/logos/ai-shifu-logo-horizontal.png';
+import logoTextColumn from 'Assets/logos/ai-shifu-logo-vertical.png';
 
 /**
  *
@@ -17,37 +13,10 @@ export const LogoWithText = ({ direction, size = 64, color = 'blue' }) => {
   const isRow = direction === 'row';
   const flexFlow = isRow ? 'row nowrap' : 'column nowrap';
 
-  const commonStyles = {
-    width: isRow ? 'auto' : size + 'px',
+  const commonStyles = { width: isRow ? 'auto' : size + 'px',
     height: isRow ? size + 'px' : 'auto',
   };
 
-  const textStyles = {
-    width: isRow ? 'auto' : size * 0.8 + 'px',
-    height: isRow ? size * 0.8 + 'px' : 'auto',
-    marginLeft: isRow ? size * 0.3 + 'px' : '0',
-    marginTop: isRow ? '0' : size * 0.3 + 'px',
-  };
-
-  const getLogoByColor = (color) => {
-    switch (color) {
-      case 'color':
-        return logoColor;
-      case 'white':
-        return logoWhite;
-      default:
-        return logoBlue;
-    }
-  };
-
-  const getTextRowByColor = (color) => {
-    switch (color) {
-      case 'white':
-        return logoTextRowWhite;
-      default:
-        return logoTextRow;
-    }
-  }
 
   return (
     <div
@@ -58,12 +27,11 @@ export const LogoWithText = ({ direction, size = 64, color = 'blue' }) => {
         ...commonStyles,
       }}
     >
-      <img src={getLogoByColor(color)} alt="logo" style={{ ...commonStyles }} />
-      {isRow ? (
-        <img src={getTextRowByColor(color)} alt="logotext" style={{ ...textStyles }} />
-      ) : (
-        <img src={logoTextColumn} alt="logotext" style={{ ...textStyles }} />
-      )}
+        {isRow ? (
+              <img src={logoTextRow} alt="logotext" style={{ ...commonStyles }} />
+            ) : (
+          <img src={logoTextColumn} alt="logotext" style={{ ...commonStyles }} />
+        )}
     </div>
   );
 };
