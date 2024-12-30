3efe063b860f77a8b740588aa29dc28b4aaa60c5
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-12-30 15:00:06 +0800

    feat: add ilivedata check

--- src/api/flaskr/api/check/__init__.py
+++ src/api/flaskr/api/check/__init__.py
@@ -1,5 +1,24 @@
 from flask import Flask
+from .ilivedata import ilivedata_check
+from .yidun import yidun_check
+from .dto import (
+    CheckResultDTO,
+    CHECK_RESULT_UNKNOWN,
+)  # noqa
 
 
 def check_text(app: Flask, data_id: str, text: str, user_id: str):
-    pass
+
+    check_provider = app.config.get("CHECK_PROVIDER")
+    if check_provider == "ilivedata":
+        return ilivedata_check(app, data_id, text, user_id)
+    elif check_provider == "yidun":
+        return yidun_check(app, data_id, text, user_id)
+    else:
+        app.logger.warning(f"check_provider {check_provider} not supported")
+        return CheckResultDTO(
+            check_result=CHECK_RESULT_UNKNOWN,
+            risk_labels=[],
+            risk_label_ids=[],
+            provider=check_provider,
+        )
