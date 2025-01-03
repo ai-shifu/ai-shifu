146db3d52ca9aeadd007af512f0ea01062fa4191
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-01-03 18:04:43 +0800

    feat: add dify set

--- src/api/flaskr/api/llm/__init__.py
+++ src/api/flaskr/api/llm/__init__.py
@@ -9,7 +9,7 @@ from langfuse.model import ModelUsage
 from openai.types.chat import ChatCompletionStreamOptionsParam
 from openai.types.shared_params import ResponseFormatJSONObject
 from flask import current_app
-
+from .dify import dify_chat_message
 from flaskr.common.config import get_config
 from flaskr.service.common.models import raise_error_with_args
 
@@ -115,6 +115,14 @@ QWEN_MODELS = [
     "qwen2-math-7b-instruct",
     "qwen2-math-1.5b-instruct",
 ]
+
+
+DIFY_MODELS = []
+
+if get_config("DIFY_API_KEY") and get_config("DIFY_URL"):
+    DIFY_MODELS = ["dify"]
+else:
+    current_app.logger.warning("DIFY_API_KEY and DIFY_URL not configured")
 
 
 class LLMStreamaUsage:
@@ -281,6 +289,19 @@ def invoke_llm(
                 res.choices[0].finish_reason,
                 None,
             )
+    elif model in DIFY_MODELS:
+        response = dify_chat_message(app, message)
+        for res in response:
+            if res.event == "message":
+                response_text += res.answer
+                yield LLMStreamResponse(
+                    res.task_id,
+                    True if res.event == "message" else False,
+                    False,
+                    res.answer,
+                    None,
+                    None,
+                )
     else:
         raise_error_with_args(
             "LLM.MODEL_NOT_SUPPORTED",
@@ -420,7 +441,19 @@ def chat_llm(
                 res.choices[0].finish_reason,
                 None,
             )
-
+    elif model in DIFY_MODELS:
+        response = dify_chat_message(app, messages[-1]["content"])
+        for res in response:
+            if res.event == "message":
+                response_text += res.answer
+                yield LLMStreamResponse(
+                    res.task_id,
+                    True if res.event == "message" else False,
+                    False,
+                    res.answer,
+                    None,
+                    None,
+                )
     else:
         raise_error_with_args(
             "LLM.MODEL_NOT_SUPPORTED",
@@ -435,4 +468,11 @@ def chat_llm(
 
 
 def get_current_models(app: Flask) -> list[str]:
-    return OPENAI_MODELS + ERNIE_MODELS + GLM_MODELS + QWEN_MODELS + DEEP_SEEK_MODELS
+    return (
+        OPENAI_MODELS
+        + ERNIE_MODELS
+        + GLM_MODELS
+        + QWEN_MODELS
+        + DEEP_SEEK_MODELS
+        + DIFY_MODELS
+    )
---
+++ src/api/flaskr/api/llm/dify.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from typing import Generator
+import requests
+import json
+
+
+class DifyChunkChatCompletionResponse:
+    event: str
+    # event : message
+    task_id: str
+    conversation_id: str
+    answer: str
+    created_at: str
+
+    def __init__(self, **kwargs):
+        self.event = kwargs.get("event", "")
+        self.task_id = kwargs.get("task_id", "")
+        self.conversation_id = kwargs.get("conversation_id", "")
+        self.answer = kwargs.get("answer", "")
+        self.created_at = kwargs.get("created_at", "")
+
+
+def dify_chat_message(
+    app: Flask, message: str
+) -> Generator[DifyChunkChatCompletionResponse, None, None]:
+    url = app.config.get("DIFY_URL")
+    api_key = app.config.get("DIFY_API_KEY")
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+    data = {"query": message, "response_mode": "streaming", "auto_generate_name": False}
+    response = requests.post(url, headers=headers, json=data, stream=True)
+    for res in response.iter_lines():
+        res = res.decode("utf-8")
+        app.logger.info("dify response data: {}".format(res))
+        if res.startswith("data:"):
+            json_data = res[5:].strip()
+            if json_data.replace(" ", "") == "[DONE]":
+                return
+            parsed_data = json.loads(json_data)
+            yield DifyChunkChatCompletionResponse(**parsed_data)
