fca5d57cab6ffff5a779bebddc7656939bb65cd8
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-01-10 20:05:48 +0800

    chroe: remove unuse code

--- src/web/src/Components/logo/LogoWithText.jsx
+++ src/web/src/Components/logo/LogoWithText.jsx
@@ -2,21 +2,18 @@ import { memo } from 'react';
 import logoRow from 'Assets/logos/ai-shifu-logo-horizontal.png';
 import logoColumn from 'Assets/logos/ai-shifu-logo-vertical.png';
 import { useEnvStore } from 'stores/envStore.js';
-// import squareLogo from 'Assets/logos/ai-shifu-logo-square.png';
 
 /**
  *
  * @param {direction} 'row' | 'col'
  * @param {size} number
- * @param { color } 'blue' | 'color' | 'white'
  * @returns
  */
-export const LogoWithText = ({ direction, size = 64, color = 'blue' }) => {
+export const LogoWithText = ({ direction, size = 64 }) => {
   const isRow = direction === 'row';
   const flexFlow = isRow ? 'row nowrap' : 'column nowrap';
   const logoHorizontal = useEnvStore((state) => state.logoHorizontal);
   const logoVertical = useEnvStore((state) => state.logoVertical);
-  console.log(logoHorizontal, logoVertical);
   const commonStyles = {
     width: isRow ? 'auto' : size + 'px',
     height: isRow ? size + 'px' : 'auto',
