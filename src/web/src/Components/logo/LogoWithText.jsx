787711aa622dadee964ee3c8f26f75aab23e8db7
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-01-10 19:58:10 +0800

    feat: add logo url to config env

--- src/web/src/Components/logo/LogoWithText.jsx
+++ src/web/src/Components/logo/LogoWithText.jsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 import logoRow from 'Assets/logos/ai-shifu-logo-horizontal.png';
 import logoColumn from 'Assets/logos/ai-shifu-logo-vertical.png';
+import { useEnvStore } from 'stores/envStore.js';
+// import squareLogo from 'Assets/logos/ai-shifu-logo-square.png';
 
 /**
  *
@@ -12,11 +14,15 @@ import logoColumn from 'Assets/logos/ai-shifu-logo-vertical.png';
 export const LogoWithText = ({ direction, size = 64, color = 'blue' }) => {
   const isRow = direction === 'row';
   const flexFlow = isRow ? 'row nowrap' : 'column nowrap';
-
+  const logoHorizontal = useEnvStore((state) => state.logoHorizontal);
+  const logoVertical = useEnvStore((state) => state.logoVertical);
+  console.log(logoHorizontal, logoVertical);
   const commonStyles = {
     width: isRow ? 'auto' : size + 'px',
     height: isRow ? size + 'px' : 'auto',
   };
+
+
 
   return (
     <div
@@ -29,9 +35,9 @@ export const LogoWithText = ({ direction, size = 64, color = 'blue' }) => {
     >
       <a href="https://ai-shifu.com">
         {isRow ? (
-          <img src={logoRow} alt="logo" style={{ ...commonStyles }} />
+          <img src={ logoHorizontal || logoRow} alt="logo" style={{ ...commonStyles }} />
         ) : (
-          <img src={logoColumn} alt="logo" style={{ ...commonStyles }} />
+          <img src={logoVertical || logoColumn} alt="logo" style={{ ...commonStyles }} />
         )}
       </a>
     </div>
