f3f4ffc3411dc05df9c7229c0432a5d987ce065b
dianshan <dianshan102@gmail.com>
2025-03-20 21:23:23 +0800

    fix merge conflict

--- src/api/flaskr/common/swagger.py
+++ src/api/flaskr/common/swagger.py
@@ -4,9 +4,7 @@ import inspect
 
 swagger_config = {
     "openapi": "3.0.2",
-    "info": {"title": "AI 师傅 - API 文档", "version": "1.0.0"},
-    "optional_fields": ["components"],
-    "tags": ["用户", "课程", "订单", "支付", "知识库"],
+    "info": {"title": "AI Shifu API", "version": "1.0.0"},
     "components": {"schemas": {}},
     "specs": [
         {
