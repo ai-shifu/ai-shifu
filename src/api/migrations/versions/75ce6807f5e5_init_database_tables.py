6c996973e3dadfbf6de34dab99121e471c86ee9c
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-04-13 00:08:51 +0800

    fix: db migrations

--- src/api/migrations/versions/75ce6807f5e5_init_database_tables.py
+++ src/api/migrations/versions/75ce6807f5e5_init_database_tables.py
@@ -201,7 +201,7 @@ def upgrade():
             comment="Course feishu ID",
         ),
         sa.Column(
-            "course_teacher_avatar",
+            "course_teacher_avator",
             sa.String(length=255),
             nullable=False,
             comment="Course teacher avatar",
--- src/api/migrations/versions/a7806e012c77_add_inital_course.py
+++ src/api/migrations/versions/a7806e012c77_add_inital_course.py
@@ -8,6 +8,7 @@ Create Date: 2024-10-28 09:25:45.438604
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
 revision = "a7806e012c77"
@@ -23,6 +24,13 @@ def upgrade():
             sa.Column(
                 "course_keywords", sa.Text(), nullable=False, comment="Course keywords"
             )
+        )
+        batch_op.alter_column(
+            "course_teacher_avator",
+            new_column_name="course_teacher_avatar",
+            existing_type=sa.String(length=255),
+            existing_nullable=False,
+            existing_server_default=text("''"),
         )
     with op.batch_alter_table("ai_lesson", schema=None) as batch_op:
         batch_op.add_column(
@@ -102,6 +110,13 @@ def upgrade():
     )
     with op.batch_alter_table("ai_course", schema=None) as batch_op:
         batch_op.drop_column("course_keywords")
+        batch_op.alter_column(
+            "course_teacher_avatar",
+            new_column_name="course_teacher_avator",
+            existing_type=sa.String(length=255),
+            existing_nullable=False,
+            existing_server_default=text("''"),
+        )
     with op.batch_alter_table("ai_lesson", schema=None) as batch_op:
         batch_op.drop_column(batch_op.f("parent_id"))
     with op.batch_alter_table("ai_lesson_script", schema=None) as batch_op:
