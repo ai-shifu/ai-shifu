44b168a3e7f865a3170a8ab0428918db95eedbf7
Sunner Sun <sunner@gmail.com>
2025-07-04 14:55:34 +0800

    fix: ask is disabled at chapter end

--- src/api/flaskr/service/study/runscript.py
+++ src/api/flaskr/service/study/runscript.py
@@ -1,5 +1,6 @@
 import traceback
 from typing import Generator
+from flaskr.service.study.ui.input_ask import handle_ask_mode
 from flask import Flask
 
 from flaskr.service.common.models import AppException, raise_error
@@ -162,6 +163,7 @@ def handle_reload_script(
             yield make_script_dto_to_stream(script_dto)
     else:
         res = update_lesson_status(app, attend.attend_id, True)
+        res.append(handle_ask_mode(app, user_info, attend, script_info, input, trace, trace_args))
         if res:
             for attend_update in res:
                 if isinstance(attend_update, AILessonAttendDTO):
@@ -511,6 +513,7 @@ def run_script_inner(
                             yield make_script_dto_to_stream(script_dto)
                     else:
                         res = update_lesson_status(app, attend.attend_id, preview_mode)
+                        res.append(handle_ask_mode(app, user_info, attend, script_info, input, trace, trace_args))
                         if res:
                             for attend_update in res:
                                 if isinstance(attend_update, AILessonAttendDTO):
