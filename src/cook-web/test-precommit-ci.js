82d7df662beb57693fb259fc93b4041b3df15493
Sunner Sun <sunner@gmail.com>
2025-07-22 12:13:36 +0800

    fix: resolve prettier timeout in pre-commit.ci
    - Remove npm ci which was causing 180s timeout
    - Use prettier directly with additional_dependencies
    - Specify prettier version to match package.json
    - Should run much faster without full npm install

--- src/cook-web/test-precommit-ci.js
+++ src/cook-web/test-precommit-ci.js
@@ -1,10 +1,10 @@
 // 测试 pre-commit.ci 自动格式化
-const   messy    =    "code"  ;
-    const data={x:1,y:2,z:3}
+const messy = 'code';
+const data = { x: 1, y: 2, z: 3 };
 
-function   badFormatting(  )   {
-console.log(   "this should be formatted"   )
-        return    true
+function badFormatting() {
+  console.log('this should be formatted');
+  return true;
 }
 
-export   default    {messy,data,badFormatting}
+export default { messy, data, badFormatting };
