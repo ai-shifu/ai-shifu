72840ae0e576c17f385d49ab41abda5730da7b33
Sunner Sun <sunner@gmail.com>
2025-09-16 20:08:03 +0800

    refactor: centralize browser language detection in web

--- src/web/src/stores/useSystemStore.ts
+++ src/web/src/stores/useSystemStore.ts
@@ -1,8 +1,9 @@
 import { create } from 'zustand';
 import { SystemStoreState } from '../types/store';
+import { browserLanguage } from '../i18n';
 
 export const useSystemStore = create<SystemStoreState>((set) => ({
-  language: typeof window !== 'undefined' ? (navigator.language || navigator.languages?.[0] || 'en-US') : 'en-US',
+  language: browserLanguage,
   channel: '',
   wechatCode: '',
   showVip: true,
