5565e7ec0ae6daf101f9e2015f888f6ebd756850
kunlulu <kunlulu810@gmail.com>
2025-11-04 20:28:18 +0800

    style: shifusetting

--- src/cook-web/src/components/shifu-edit/ShifuEdit.tsx
+++ src/cook-web/src/components/shifu-edit/ShifuEdit.tsx
@@ -216,7 +216,7 @@ const ScriptEditor = ({ id }: { id: string }) => {
           </div>
 
           {!foldOutlineTree && (
-            <div className='flex-1 h-full overflow-y-auto overflow-x-hidden w-[256px]'>
+            <div className='flex-1 h-full overflow-y-auto overflow-x-hidden w-[256px] pb-10'>
               <ol className=' text-sm'>
                 <OutlineTree
                   items={chapters}
