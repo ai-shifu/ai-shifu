bcb9ecc027eea39e48853e9236a5773c328c59c5
kunlulu <kunlulu810@gmail.com>
2025-11-06 15:14:31 +0800

    fix: lint error

--- src/cook-web/src/components/shifu-setting/ShifuSetting.tsx
+++ src/cook-web/src/components/shifu-setting/ShifuSetting.tsx
@@ -329,7 +329,7 @@ export default function ShifuSettingDialog({
         <div className='h-px w-full bg-border' />
         <Form {...form}>
           <form
-            onSubmit={form.handleSubmit(onSubmit)}
+            onSubmit={form.handleSubmit((data) => onSubmit(data, true))}
             className='flex-1 flex flex-col overflow-hidden'
           >
             <div className='flex-1 overflow-y-auto px-6'>
