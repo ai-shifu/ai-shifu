43cae8bdb39a5e450247caa2a17d7e51d63f017e
kunlulu <59993024+kunlulu@users.noreply.github.com>
2025-11-08 20:20:24 +0800

    fix(logo): use config homeUrl for logo link (#901)
    Co-authored-by: kunlulu <kunlulu810@gmail.com>

--- src/cook-web/src/components/shifu-edit/ShifuEdit.tsx
+++ src/cook-web/src/components/shifu-edit/ShifuEdit.tsx
@@ -43,7 +43,7 @@ const initializeEnvData = async (): Promise<void> => {
     updateLogoHorizontal,
     updateLogoVertical,
     updateEnableWxcode,
-    updateSiteUrl,
+    updateHomeUrl,
   } = useEnvStore.getState() as EnvStoreState;
 
   const fetchEnvData = async (): Promise<void> => {
@@ -66,7 +66,7 @@ const initializeEnvData = async (): Promise<void> => {
         await updateLogoHorizontal(data?.logoHorizontal || '');
         await updateLogoVertical(data?.logoVertical || '');
         await updateEnableWxcode(data?.enableWechatCode?.toString() || 'true');
-        await updateSiteUrl(data?.siteHost || '');
+        await updateHomeUrl(data?.homeUrl || '');
       }
     } catch (error) {
       console.error(error);
