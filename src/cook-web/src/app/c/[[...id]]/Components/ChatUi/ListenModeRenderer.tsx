84f7e8ae42815cd7c0901cb7e31d01ef65af279d
kunlulu <kunlulu810@gmail.com>
2026-02-02 16:42:24 +0800

    fix(chat): hide non-present listen slides to prevent sandbox overlap

--- src/cook-web/src/app/c/[[...id]]/Components/ChatUi/ListenModeRenderer.tsx
+++ src/cook-web/src/app/c/[[...id]]/Components/ChatUi/ListenModeRenderer.tsx
@@ -260,6 +260,7 @@ const ListenModeRenderer = ({
           {!isLoading &&
             slideItems.map(({ item, segments }, idx) => {
               const baseKey = item.generated_block_bid || `${item.type}-${idx}`;
+              // console.log('segments', baseKey, segments)
               return (
                 <ContentIframe
                   key={baseKey}
