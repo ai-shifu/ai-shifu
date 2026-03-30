8c58ec3dca2b87827f01f0db62167347e3609daa
yfge <geyunfei@gmail.com>
2026-03-30 23:40:52 +0800

    refactor: inline order helper wrappers

--- src/api/flaskr/service/order/query_discount.py
+++
@@ -1,5 +0,0 @@
-from flaskr.service.promo.models import CouponUsage as CouponUsageModel
-
-
-def query_discount_record(order_id: str) -> list[CouponUsageModel]:
-    return CouponUsageModel.query.filter(CouponUsageModel.order_bid == order_id).all()
